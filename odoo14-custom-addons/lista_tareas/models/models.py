5b9844e0f803bf3b1e95b7c1eefeff9437fba0fc
Enrique <enriquegpb5@gmail.com>
2022-02-11 10:00:18 +0100

    desarrollo de modulo prueba

--- odoo14-custom-addons/lista_tareas/models/models.py
+++ odoo14-custom-addons/lista_tareas/models/models.py
@@ -11,11 +11,12 @@ class lista_tareas(models.Model):
     #Elementos de cada fila del modelo de datos
     #Los tipos de datos a usar en el ORM son 
     # https://www.odoo.com/documentation/14.0/developer/reference/addons/orm.html#fields
-   
+    avatar=fields.Image('Imagen tarea',max_width=50,max_heigth=50)
     tarea = fields.Char()
     prioridad = fields.Integer()
     urgente = fields.Boolean(compute="_value_urgente", store=True)
     realizada = fields.Boolean()
+    
 
 
     #Este computo depende de la variable prioridad
