092bee3f2a6e09d036de10ae869d9d3c462d8bd9
Enrique <enriquegpb5@gmail.com>
2022-02-21 00:33:25 +0100

    desarrollo modulos con nuevos widgets y estructuras de datos

--- odoo14-custom-addons/lista_tareas/models/models.py
+++ odoo14-custom-addons/lista_tareas/models/models.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from cgi import FieldStorage
 from odoo import models, fields, api
+import time
+from datetime import datetime, date
+
 
 #Definimos el modelo de datos
 class lista_tareas(models.Model):
     #Nombre y descripcion del modelo de datos
     _name = 'lista_tareas.lista_tareas'
     _description = 'lista_tareas.lista_tareas'
-
     #Elementos de cada fila del modelo de datos
     #Los tipos de datos a usar en el ORM son 
     # https://www.odoo.com/documentation/14.0/developer/reference/addons/orm.html#fields
@@ -16,6 +19,17 @@ class lista_tareas(models.Model):
     prioridad = fields.Integer()
     urgente = fields.Boolean(compute="_value_urgente", store=True)
     realizada = fields.Boolean()
+    fecha = fields.Date()
+    progreso=fields.Selection(
+        String="Progreso",
+        selection=[
+            ('undone',"Sin Hacer"),
+            ('progres',"haciendo"),
+            ('done',"hecho"),
+        ]
+
+    )
+    
     
 
 
@@ -30,4 +44,13 @@ class lista_tareas(models.Model):
                 record.urgente = True
             else:
                 record.urgente = False
+    
+    
+    
+
+
+
+   
+
+
 
